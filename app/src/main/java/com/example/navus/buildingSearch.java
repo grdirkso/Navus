8e8f15763ab068b8ad426df7655a492ce4dec878
Gillian Dirkson <36120935+grdirkso@users.noreply.github.com>
2020-12-12 20:33:56 -0600

    Revert "Directions implementation"

--- app/src/main/java/com/example/navus/buildingSearch.java
+++ app/src/main/java/com/example/navus/buildingSearch.java
@@ -2,7 +2,9 @@ package com.example.navus;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.media.Image;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
@@ -11,12 +13,10 @@ import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Spinner;
 
-public class BuildingSearch extends AppCompatActivity {
+public class buildingSearch extends AppCompatActivity {
 
-    public Object item = new Object();
-    public int selectedItem = 0;
-    private String building;
-
+    private Object item = new Object();
+    private int selectedItem = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -32,23 +32,20 @@ public class BuildingSearch extends AppCompatActivity {
         );
         buildings.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         buildingPicker.setAdapter(buildings);
-        buildingPicker.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
 
+        buildingPicker.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
                 item = adapterView.getItemAtPosition(i);
 
                 if(item == adapterView.getItemAtPosition(0)) {
                     selectedItem = 0;
-                    building = "bell";
                     buildingImg.setImageResource(R.drawable.bell);
                 } else if(item == adapterView.getItemAtPosition(1)) {
                     selectedItem = 1;
-                    building = "JBH";
                     buildingImg.setImageResource(R.drawable.jbht);
                 } else if (item == adapterView.getItemAtPosition(2)){
                     selectedItem = 2;
-                    building = "white";
                     buildingImg.setImageResource(R.drawable.sen);
                 }
             }
@@ -61,17 +58,11 @@ public class BuildingSearch extends AppCompatActivity {
         getDirections.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                System.out.println(building);
-                Intent intent = new Intent(BuildingSearch.this, MapsActivity.class);
-                intent.putExtra("building", building);
-                startActivity(intent);
+
+                Intent intent = new Intent(buildingSearch.this, MapsActivity.class);
+                intent.putExtra("building", selectedItem);
+                startActivityForResult(intent, selectedItem);
             }
         });
     }
 }
-
-
-
-
-// if (id = 0)
-//    "https://maps.googleapis.com/maps/api/directions/json?origin=" + CurrentLocation + "&destination=" + Location0 + "&key=API_KEY"
