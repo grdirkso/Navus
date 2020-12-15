a7726612a88874248c8c344de8d8be80476bd22b
grdirkso <36120935+grdirkso@users.noreply.github.com>
2020-12-14 23:33:54 -0600

    the final touches

--- app/src/main/java/com/example/navus/MapsActivity.java
+++ app/src/main/java/com/example/navus/MapsActivity.java
@@ -1,5 +1,6 @@
 package com.example.navus;
 
+import androidx.annotation.NonNull;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 import androidx.fragment.app.FragmentActivity;
@@ -8,34 +9,60 @@ import androidx.fragment.app.FragmentTransaction;
 import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
 import android.location.Location;
+import android.location.LocationListener;
+import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
 import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.PolylineOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.maps.DirectionsApi;
+import com.google.maps.GeoApiContext;
+import com.google.maps.android.PolyUtil;
+import com.google.maps.errors.ApiException;
+import com.google.maps.model.DirectionsResult;
+import com.google.maps.model.TravelMode;
 
+import java.io.IOException;
+import java.time.Instant;
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
+import java.util.Locale;
+import java.util.concurrent.TimeUnit;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
     public BuildingSearch buildingSearch;
     private String building;
-    private FusedLocationProviderClient client;
     private static final int LOCATION_PERMISSION_REQUEST_CODE = 1;
+    private LatLng curLoc;
+    private LatLng geoDestination;
+    private FusedLocationProviderClient mFusedLocationClient;
+    private Double lat;
+    private Double lng;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
         setContentView(R.layout.activity_main);
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
@@ -52,39 +79,64 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         building = this.getIntent().getStringExtra("building");
         final ArrayList<ClassRoom> myPostList = (ArrayList<ClassRoom>) this.getIntent().getSerializableExtra("json");
         Button hereBtn = findViewById(R.id.here);
+        Button directionsBtn = findViewById(R.id.mapsBtn);
 
         hereBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Log.d("maps: " , "here button clicked");
+                Log.d("maps: ", "here button clicked");
                 Intent intent = new Intent(getApplicationContext(), ClassPath.class);
                 intent.putExtra("json", myPostList);
                 intent.putExtra("building", building);
                 startActivity(intent);
             }
         });
+
+        directionsBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                getBuilding();
+                Log.d("maps: ",geoDestination.latitude + "," + geoDestination.longitude );
+                String geo =  "https://maps.google.com/maps?daddr="+geoDestination.latitude + "," + geoDestination.longitude;
+                Uri gmmIntentUri = Uri.parse(geo);
+                Intent mapIntent = new Intent(Intent.ACTION_VIEW, gmmIntentUri);
+                mapIntent.setPackage("com.google.android.apps.maps");
+                if(mapIntent.resolveActivity(getPackageManager()) != null) {
+                    startActivity(mapIntent);
+                }
+
+            }
+        });
+
     }
+
 
     /**
      * Will get the last known location
      */
     private void getLocationAndLog() {
-        if (client != null) {
+        if (mFusedLocationClient != null) {
             if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
                 Log.d("MapsActivity", "No Location Permission");
                 return;
             }
-            client.getLastLocation().addOnSuccessListener(this, new OnSuccessListener<Location>() {
+            mFusedLocationClient.getLastLocation().addOnSuccessListener(this, new OnSuccessListener<Location>() {
                 @Override
                 public void onSuccess(Location location) {
                     if (location != null) {
+                        lat = location.getLatitude();
+                        lng = location.getLongitude();
                         LatLng curLoc = new LatLng(location.getLatitude(), location.getLongitude());
-                        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(curLoc,11));
+                        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(curLoc, 15));
+                        directions();
                     }
                 }
             });
         }
+        //load all picture items from db
     }
+
+
     /**
      * Enables the My Location layer if the fine location permission has been granted.
      */
@@ -119,29 +171,104 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         //get last known location
         getLocationAndLog();
 
-        switch(building) {
+        getBuilding();
+
+    }
+
+
+    public void getBuilding() {
+        switch (building) {
             case "JBH":
-                System.out.println(building);
                 // Add a marker at the J.B. Hunt building and move the camera
                 LatLng jbhunt = new LatLng(36.06622685758442, -94.17374858900389);
                 mMap.addMarker(new MarkerOptions().position(jbhunt).title("J.B. Hunt Center for Academic Excellence"));
                 mMap.moveCamera(CameraUpdateFactory.newLatLng(jbhunt));
+                geoDestination = jbhunt;
                 break;
             case "bell":
                 // Add a marker at Bell Engineering and move the camera
                 LatLng bell = new LatLng(36.0671895009137, -94.17138824515493);
                 mMap.addMarker(new MarkerOptions().position(bell).title("Bell Engineering Center"));
                 mMap.moveCamera(CameraUpdateFactory.newLatLng(bell));
+                geoDestination = bell;
                 break;
             case "white":
                 // Add a marker at White Engineering Hall and move the camera
                 LatLng white = new LatLng(36.06712139893772, -94.17055018102211);
                 mMap.addMarker(new MarkerOptions().position(white).title("John White Jr. Engineering Hall"));
                 mMap.moveCamera(CameraUpdateFactory.newLatLng(white));
+                geoDestination = white;
                 break;
             default:
                 System.out.println("not working");
         }
+    }
+
+    public void directions() {
+
+        Log.d("Maps: ", "directons");
+        Instant now = null;
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+            now = Instant.now();
+        }
+        String origin = getCompleteAddressString(lat, lng);
+        String destination = getCompleteAddressString(geoDestination.latitude, geoDestination.longitude);
+        try {
+            DirectionsResult result = DirectionsApi.newRequest(getGeoContext()).mode(TravelMode.DRIVING).origin(origin)
+                    .destination(destination)
+                    .departureTime(now).await();
+            addPolyline(result, mMap);
+        } catch (ApiException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private GeoApiContext getGeoContext() throws InterruptedException, ApiException, IOException {
+        return new GeoApiContext.Builder()
+                .queryRateLimit(1)
+                .apiKey("AIzaSyC_moMjS9Bjbzsn7MwNBhIfT0kS33a5r-w")
+                .connectTimeout(1, TimeUnit.SECONDS)
+                .readTimeout(1, TimeUnit.SECONDS)
+                .writeTimeout(1, TimeUnit.SECONDS)
+                .build();
+    }
+
+    private String getCompleteAddressString(double LATITUDE, double LONGITUDE) {
+        String strAdd = "";
+        Geocoder geocoder = new Geocoder(this, Locale.getDefault());
+        try {
+            List<Address> addresses = geocoder.getFromLocation(LATITUDE, LONGITUDE, 1);
+            if (addresses != null) {
+                Address returnedAddress = addresses.get(0);
+                StringBuilder strReturnedAddress = new StringBuilder("");
+
+                for (int i = 0; i <= returnedAddress.getMaxAddressLineIndex(); i++) {
+                    strReturnedAddress.append(returnedAddress.getAddressLine(i)).append("\n");
+                }
+                strAdd = strReturnedAddress.toString();
+                Log.w("", strReturnedAddress.toString());
+            } else {
+                Log.w("", "No Address returned!");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            Log.w("", "Canont get Address!");
+        }
+        return strAdd;
+
+    }
+
+    public void directions(double lat, double lng){
+
+    }
+
+    private void addPolyline(DirectionsResult results, GoogleMap mMap) {
+        List<LatLng> decodedPath = PolyUtil.decode(results.routes[0].overviewPolyline.getEncodedPath());
+        mMap.addPolyline(new PolylineOptions().addAll(decodedPath));
     }
 }
 
